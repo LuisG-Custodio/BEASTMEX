53072526988e3a337f7ac2bc9d3467ba39b17d3b
SamanthaArteaga <121037868@upq.edu.mx>
2023-11-03 11:48:22 -0600

    Login
    Hice los cambios en el login, espero confirmación de aprobado o que le cambio

--- Importaciones_Beastmex/resources/views/login.blade.php
+++ Importaciones_Beastmex/resources/views/login.blade.php
@@ -1,24 +1,48 @@
-@extends('layouts.template')
-@section('titulo','Login')
-@section('contenido')
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <style>
 
-<h1 class="text-center">LOGIN</h1>
-<div class="container mt-2 col-md-2">
-<form>
-    <div class="mb-3">
-      <label for="exampleInputEmail1" class="form-label">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-     
+      .card-body, .form-label, .form-control, .form-check-label, .btn {
+          font-family: Arial, sans-serif; 
+      }
+  </style>
+</head>
+<body style="background-color: #517272;">
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+    <div class="container mt-2 col-md-2">
+        <div class="card">
+            <div class="card-body">
+                <h1 class="text-center">LOGIN</h1>
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Recuerdame</label>
+                    </div>
+                    <div class="text-center">
+                    <button type="submit" class="btn btn-outline-secondary w-100">Iniciar</button>
+                    </div>
+                  </form>
+
+            </div>
+        </div>
     </div>
-    <div class="mb-3">
-      <label for="exampleInputPassword1" class="form-label">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1">
-    </div>
-    <div class="mb-3 form-check">
-      <input type="checkbox" class="form-check-input" id="exampleCheck1">
-      <label class="form-check-label" for="exampleCheck1">Check me out</label>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
-</div>
-@endsection
+</body>
+</html>
