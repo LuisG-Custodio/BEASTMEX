46817cc68ab325da32790f40538b5ed250715704
LuisG-Custodio <121039302@upq.edu.mx>
2023-11-02 22:42:15 -0600

    Creacion inicial de formularios

---
+++ Importaciones_Beastmex/resources/views/partials/forms/form_proveedor.blade.php
@@ -0,0 +1,45 @@
+<form action="" method="POST">
+    @csrf
+    <div class="row g-3">
+        <div class="col-md-6">
+            <label for="inputNombre" class="form-label">Nombre de la Empresa</label>
+            <input type="text" class="form-control" id="inputNombreEmpresa" name="_NombreEmpresa" required>
+        </div>
+        <div class="col-md-6">
+            <label for="inputTelefono" class="form-label">RFC</label>
+            <input type="text" class="form-control" id="inputRFC" name="_RFC" required>
+        </div>
+    </div>
+
+    <div class="row g-3">
+        <div class="col-md-6">
+            <label for="inputTelefono" class="form-label">Teléfono de Contacto</label>
+            <input type="text" class="form-control" id="inputTelefono" name="_Telefono" required>
+        </div>
+        <div class="col-md-6">
+            <label for="inputCorreo" class="form-label">Correo de Contacto</label>
+            <input type="email" class="form-control" id="inputCorreo" name="_Correo" required>
+        </div>
+    </div>
+
+    <div class="row g-3">
+        <div class="col-md-12">
+            <label for="inputDireccion" class="form-label">Dirección de la Empresa</label>
+            <textarea class="form-control" wrap="hard" id="inputDireccion" rows="3" name="_Direccion"></textarea>
+        </div>
+    </div>
+
+    <div class="row g-3">
+        <div class="col-md-12">
+            <label for="inputProductos" class="form-label">Descripción de Productos Ofrecidos</label>
+            <textarea class="form-control" wrap="hard" id="inputProductos" rows="3" name="_Productos"></textarea>
+        </div>
+    </div>
+
+    <div class="row g-3">
+        <div class="col-md-12">
+            <label for="inputNotas" class="form-label">Notas Adicionales</label>
+            <textarea class="form-control" wrap="hard" id="inputNotas" rows="3" name="_Notas"></textarea>
+        </div>
+    </div>
+</form>
